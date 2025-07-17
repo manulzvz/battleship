f36bcb7b5ee7d20f060b0d75ca5909e7f2166c73
manulzvz13 <manulzvz@gmail.com>
2025-07-17 17:09:50 -0600

    feat: Configure webpack and initial structure for webversion

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Player from "./player.js";
+import Computer from "./computer.js";
+import Ship from "./ship.js";
+import "./style.css";
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './public',
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
