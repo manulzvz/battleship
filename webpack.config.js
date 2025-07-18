b7106515f9b29e3226fc9fba33ccf06ac2885ae1
manulzvz13 <manulzvz@gmail.com>
2025-07-18 09:28:27 -0600

    fix(plugin): Add webpack HTML plugin and template

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 import path from 'path';
 import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,6 +27,12 @@ export default {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Usa tu archivo HTML como base
+      filename: 'index.html'
+    }),
+  ],
   experiments: {
     topLevelAwait: true,
   },
